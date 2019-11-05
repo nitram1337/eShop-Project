d36dfc638a086c4664460479e04eac872514544c
Martin Sonne <Martin Sonne@LAPTOP-JULDTJEK>
2019-11-05 09:09:32 +0100

    Tilføjet swagger

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -16,6 +16,7 @@ using ServiceLayer.OrderService.Abstract;
 using ServiceLayer.OrderService.Concrete;
 using ServiceLayer.ProductService.Abstract;
 using ServiceLayer.ProductService.Concrete;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPI
 {
@@ -36,11 +37,26 @@ namespace WebAPI
             services.AddScoped<IDeliveryPaymentDropdownService, DeliveryPaymentDropdownService>();
             services.AddScoped<IListOrderService, ListOrderService>();
             services.AddDbContext<EshopContext>(options => options.UseSqlServer(Configuration.GetConnectionString("WebShop")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
